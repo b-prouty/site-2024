8f05b8b6cdae72cf179da91804748e984582681e
Brian Prouty <brianproutydesign@gmail.com>
2024-06-24 09:30:21 -0700

    Work + Person Web finished

--- js/main.js
+++ js/main.js
@@ -5,21 +5,51 @@ const purple = "#CB8BD0";
 const yellow = "#E2C541";
 const red = "#ED6262";
 
-function enterPage(){
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
+
+function enterPage() {
     pageEnter.classList.add('trans-down');
 }
 
-function pageTrans(color, href){
+function pageTrans(color, href) {
     pageTransition.style.backgroundColor = color;
     pageTransition.classList.add('trans-up');
 
     setTimeout(() => {
-        window.location.href = href + ".html";
+        window.location.href = href.toLowerCase() + ".html";
     }, 1000);
-   
+
 }
 
-window.onload = function() {
+window.onload = function () {
+    includeHTML();
+    
     setTimeout(() => {
         enterPage();
     }, 1000);
