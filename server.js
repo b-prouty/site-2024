fc3142eaaae23a539516052ed7b219a6fc46a57c
Brian Prouty <brianproutydesign@gmail.com>
2025-02-10 17:51:12 -0800

    bugs

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const path = require('path');
 // Load environment variables
 require('dotenv').config();
 const { OpenAI } = require('openai');
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,,
-});
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 const fs = require('fs');
 
 
