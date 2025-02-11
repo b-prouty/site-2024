a400d9789e6040ab49065f79de26d5ea0c2aa576
Brian Prouty <brianproutydesign@gmail.com>
2025-02-10 17:08:45 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -33,11 +33,12 @@
         return;
       }
       
-    const response = await fetch('/api/ask', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ question }),
-    });
+      const response = await fetch('https://staging.brianprouty.com/ask', {  // Remove /api/ from the path
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ question }),
+      });
+      
     const data = await response.json();
     // console.log(data.answer); 
     console.log('Data:', data);
