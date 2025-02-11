5690497a5d21d0808914ffbef1a49898fba09693
Brian Prouty <brianproutydesign@gmail.com>
2025-02-10 17:13:28 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,31 +1,4 @@
-// async function askBrian() {
-//     const question = document.getElementById('question').value;
-  
-//     if (!question) {
-//       alert('Please enter a question.');
-//       return;
-//     }
-  
-//     // const response = await fetch('/ask', {
-//     //   method: 'POST',
-//     //   headers: { 'Content-Type': 'application/json' },
-//     //   body: JSON.stringify({ question }),
-//     // });
-
-//     const response = await fetch('/api/ask', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({ question }),
-//       })
-  
-//     const data = await response.json();
-//     console.log('Data:', data);
-//     document.getElementById('answer').innerText = data.answer || 'No answer available.';
-
-    
-//   }
-
-  async function askBrian() {
+async function askBrian() {
     const question = document.getElementById('question').value;
 
     if (!question) {
